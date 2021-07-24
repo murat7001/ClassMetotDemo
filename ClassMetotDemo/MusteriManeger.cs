2492b53748daccbb1e7977463dc5648f21e63d20
Murat Yıldırım <2020555068@ogr.cu.edu.tr>
2021-07-24 11:20:55 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManeger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManeger
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Eklendi:" + " " + musteri.Adi +" "+ musteri.SoyAdi);
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Silindi :" + " " + musteri.Adi +" "+ musteri.SoyAdi);
+        }
+
+        public void Listele(Musteri[] musteriler)
+        {
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Adi+" "+musteri.SoyAdi);
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Adi = "Murat";
+            musteri1.SoyAdi = "Yıldırım";
+            musteri1.DogumTarihi = 2002;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Adi = "Çağrı";
+            musteri2.SoyAdi = "Türkmen";
+            musteri2.DogumTarihi = 2002;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Adi = "Engin";
+            musteri3.SoyAdi = "Demiroğ";
+            musteri3.DogumTarihi = 1985;
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            MusteriManeger musteriManeger = new MusteriManeger();
+            musteriManeger.Ekle(musteri1);
+            musteriManeger.Sil(musteri2);
+            musteriManeger.Listele(musteriler);
+
+        }
+    }
+}
